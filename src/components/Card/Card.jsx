411b993eac123df4c4b66851bdd6ce9512de5961
Gleice Sabrina Azevedo <84392497+gleicesabrina@users.noreply.github.com>
2024-01-30 12:54:38 +0000

    Criação e estilização das tags

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,30 @@
+import './Card.css';
+
 function Card () {
     return(
         <>
+
         <div className="card">
             <h2>Rick Sanchez</h2>
+
+                <div className="tags">
+                    <div className='tag'> Status: Vivo</div> 
+                    <div className='tag'>Especie: Humano</div>   
+                    <div className='tag'>Origem: Terra C-137</div> 
+                </div>
+
             <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
+       </div>
+       <div className="card">
+            <h2>Morty Smith </h2>
+
+                <div className="tags">
+                    <div className='tag'> Status: Vivo</div> 
+                    <div className='tag'>Especie: Humano</div>   
+                    <div className='tag'>Origem: Terra C-137</div>
+                </div> 
+                
+            <img src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"/>
        </div>
        </>
     )
