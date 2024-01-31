86b7a8c6f6bc9cfbf1afd93fad950ef096c06300
Gleice Sabrina Azevedo <84392497+gleicesabrina@users.noreply.github.com>
2024-01-31 15:01:53 +0000

    ´useEffect´ adicionado para previnir DDOS

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,58 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Card from './components/Card/Card';
 import Tag from './components/Tag/Tag'
 
 function App() {
+  //TODO: TERMINAR A PARTE DE TAGS 
   //Carregamento de dados via Código
-  const item1 = {
-    name: 'Rick Sanchez',
-    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
-  }
-  const item2 = {
-    name: 'Morty Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
-  }
-  const item3 = {
-    name: 'Summer Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
-  }
-  const item4 = {
-    name: 'Beth Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg'
-  }
+  // const item1 = {
+  //   name: 'Rick Sanchez',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  // }
+  // const item2 = {
+  //   name: 'Morty Smith',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  // }
+  // const item3 = {
+  //   name: 'Summer Smith',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+  // }
+  // const item4 = {
+  //   name: 'Beth Smith',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg'
+  // }
+  
+  //Adicionando as tag  
+  // const list1 = {
+  //     status: 'Status: Vivo',
+  //     especie: 'Especie: Humano',
+  //     origem: 'Origem: Terra C-137'
+  // }
+  // const list2 = {
+  //       status: 'Status: Vivo',
+  //       especie: 'Especie: Humano',
+  //       origem: 'Origem: Terra C-137'
+  // }
+  // const list3 = {
+  //         status: 'Status: Vivo',
+  //         especie: 'Especie: Humano',
+  //         origem: 'Origem: Terra C-137'
+  // }
+  // const list4 = {
+  //           status: 'Status: Vivo',
+  //           especie: 'Especie: Humano',
+  //           origem: 'Origem: Terra C-137'
+  // }
+  // lista de tags
+  // const lists = [list1, list2, list3, list4]
+  //TO 
+  
+  
 
-  // Carregamento de dados via API (Backend) 
+  const [itens, setItens] =  useState([])
+
+   // Carregamento de dados via API (Backend)  
 async function carregarDadosApi(){
 
   const apiUrl = 'https://rickandmortyapi.com/api/character/'
@@ -31,38 +62,20 @@ async function carregarDadosApi(){
     //console.log(response)
 
     const body = await response.json()
-  console.log(body)
+    console.log(body)
 
   // Extraindo a proporiedade results do body
   // Essa propriedade contém a lista de itens
+  const results = body.results
+  console.log(results)
 
-  
+  setItens(results)
 }
-
+useEffect(function(){
+  
+  //Chamando a função que carrega dados da API
   carregarDadosApi()
-  // const list1 = {
-  //   status: 'Status: Vivo',
-  //   especie: 'Especie: Humano',
-  //   origem: 'Origem: Terra C-137'
-  // }
-  // const list2 = {
-  //   status: 'Status: Vivo',
-  //   especie: 'Especie: Humano',
-  //   origem: 'Origem: Terra C-137'
-  // }
-  // const list3 = {
-  //   status: 'Status: Vivo',
-  //   especie: 'Especie: Humano',
-  //   origem: 'Origem: Terra C-137'
-  // }
-  // const list4 = {
-  //   status: 'Status: Vivo',
-  //   especie: 'Especie: Humano',
-  //   origem: 'Origem: Terra C-137'
-  // }
-
-  const itens = [item1, item2, item3, item4]
-  //const lists = [list1, list2, list3, list4]
+}, [])
 
   return (
     <>
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -13,11 +13,13 @@ function Card(props) {
         <div className="card">
             <h2>{item.name}</h2>
 
+            
+                {/*//TODO: Arrumar as tags  
                 <div className="tags">
                     <div className='tag'>Status: Vivo</div> 
                     <div className='tag'>Especie: Humano</div>   
                     <div className='tag'>Origem: Terra C-137</div> 
-                </div>
+                </div> */}
 
             <img src={item.image}/>
        </div>
