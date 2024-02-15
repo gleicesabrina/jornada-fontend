067f4f0db3b86424eb468e75b4e8dbe643ed5ba5
Gleice Sabrina Azevedo <84392497+gleicesabrina@users.noreply.github.com>
2024-02-15 02:33:27 +0000

    Adicionando as ´Tags´ aos cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,11 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import Card from './components/Card/Card';
-import Tag from './components/Tag/Tag'
+
 
 function App() {
-  //TODO: TERMINAR A PARTE DE TAGS 
-  //Carregamento de dados via Código
-  // const item1 = {
-  //   name: 'Rick Sanchez',
-  //   image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
-  // }
-  // const item2 = {
-  //   name: 'Morty Smith',
-  //   image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
-  // }
-  // const item3 = {
-  //   name: 'Summer Smith',
-  //   image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
-  // }
-  // const item4 = {
-  //   name: 'Beth Smith',
-  //   image: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg'
-  // }
+ 
   
-  //Adicionando as tag  
-  // const list1 = {
-  //     status: 'Status: Vivo',
-  //     especie: 'Especie: Humano',
-  //     origem: 'Origem: Terra C-137'
-  // }
-  // const list2 = {
-  //       status: 'Status: Vivo',
-  //       especie: 'Especie: Humano',
-  //       origem: 'Origem: Terra C-137'
-  // }
-  // const list3 = {
-  //         status: 'Status: Vivo',
-  //         especie: 'Especie: Humano',
-  //         origem: 'Origem: Terra C-137'
-  // }
-  // const list4 = {
-  //           status: 'Status: Vivo',
-  //           especie: 'Especie: Humano',
-  //           origem: 'Origem: Terra C-137'
-  // }
-  // lista de tags
-  // const lists = [list1, list2, list3, list4]
-  //TO 
-  
-  
-
   const [itens, setItens] =  useState([])
 
    // Carregamento de dados via API (Backend)  
@@ -70,6 +26,7 @@ async function carregarDadosApi(){
   console.log(results)
 
   setItens(results)
+ 
 }
 useEffect(function(){
   
@@ -82,9 +39,7 @@ useEffect(function(){
       <div className="cards">
        
         {itens.map((item,i) => <Card item={item} key={i} />)}
-
-       {/*{lists.map(list => <Tag list={list} />)} */}
-
+       
       </div>
     </>
   )
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,26 +1,24 @@
 import './Card.css';
 
+
 function Card(props) {
-    //console.log(props)
 
     const item = props.item
-
-    //console.log(item)
+    
    
     return(
         <>
 
         <div className="card">
             <h2>{item.name}</h2>
-
             
-                {/*//TODO: Arrumar as tags  
-                <div className="tags">
-                    <div className='tag'>Status: Vivo</div> 
-                    <div className='tag'>Especie: Humano</div>   
-                    <div className='tag'>Origem: Terra C-137</div> 
-                </div> */}
-
+            <div className="tags">
+                <div className='tag'>{item.status}</div>
+                <div className='tag'>{item.gender}</div>
+                <div className='tag'>{item.species}</div>
+                <div className='tag'>{item.origin.name}</div>
+            </div>
+        
             <img src={item.image}/>
        </div>
        </>
